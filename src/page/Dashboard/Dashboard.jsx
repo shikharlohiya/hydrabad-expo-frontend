e9a428085ee163a16df72b32eb0fecc90ee6b723
Satendra Kumar <satendrakm27@gmail.com>
2025-07-17 17:16:46 +0530

    i frame added

--- src/page/Dashboard/Dashboard.jsx
+++ src/page/Dashboard/Dashboard.jsx
@@ -8,38 +8,43 @@ import OutgoingCallPage from "./OutgoingCall/OutgoingCallPage";
 import ContactsPage from "./Contacts/ContactsPage";
 import CallHistoryPage from "./CallHistory/CallHistoryPage";
 import ProfilePage from "./Profile/ProfilePage";
+import CallingPage from "./CallingPage/CallingPage";
 
 const Dashboard = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<DashboardLayout />}>
-                {/* Default dashboard route */}
-                <Route index element={<DashboardPage />} />
+  return (
+    <Routes>
+      <Route path="/" element={<DashboardLayout />}>
+        {/* Default dashboard route */}
+        <Route index element={<DashboardPage />} />
 
-                {/* Dashboard sub-routes */}
-                <Route path="home" element={<DashboardPage />} />
-                <Route path="incoming-call" element={<IncomingCallPage />} />
-                <Route path="outgoing-call" element={<OutgoingCallPage />} />
-                <Route path="contacts" element={<ContactsPage />} />
-                <Route path="call-history" element={<CallHistoryPage />} />
-                <Route path="profile" element={<ProfilePage />} />
+        {/* Dashboard sub-routes */}
+        <Route path="home" element={<DashboardPage />} />
+        <Route path="incoming-call" element={<IncomingCallPage />} />
+        <Route path="outgoing-call" element={<OutgoingCallPage />} />
+        <Route path="contacts" element={<ContactsPage />} />
+        <Route path="call-history" element={<CallHistoryPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="calling-page" element={<CallingPage />} />
 
-                {/* Add more dashboard routes here as needed */}
-                {/* Example additional routes:
+        {/* Add more dashboard routes here as needed */}
+        {/* Example additional routes:
                 <Route path="trades" element={<TradesPage />} />
                 <Route path="settings" element={<SettingsPage />} />
                 <Route path="profile" element={<ProfilePage />} />
                 <Route path="reports" element={<ReportsPage />} />
                 */}
 
-                {/* Redirect /dashboard/dashboard to /dashboard */}
-                <Route path="dashboard" element={<Navigate to="/dashboard" replace />} />
+        {/* Redirect /dashboard/dashboard to /dashboard */}
+        <Route
+          path="dashboard"
+          element={<Navigate to="/dashboard" replace />}
+        />
 
-                {/* 404 for dashboard sub-routes */}
-                <Route path="*" element={<NotFound />} />
-            </Route>
-        </Routes>
-    );
+        {/* 404 for dashboard sub-routes */}
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default Dashboard;
