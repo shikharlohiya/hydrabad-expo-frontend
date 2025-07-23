2d45d21fc9b44c97ea1b7075a14fb69b7f075ee8
Satendra Kumar <satendrakm27@gmail.com>
2025-07-23 21:35:20 +0530

    iframe and calling pages removed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import AuthContext from "./context/AuthContext";
 import Login from "./page/Login";
 import NotFound from "./page/NotFound";
 import Dashboard from "./page/Dashboard/Dashboard";
-import CallingPage from "./page/Dashboard/CallingPage/CallingPage";
 
 function App() {
   const { isAuthenticated } = useContext(AuthContext);
@@ -38,7 +37,6 @@ function App() {
               </PublicRoute>
             }
           />
-          <Route path="calling-page" element={<CallingPage />} />
 
           {/* Protected Dashboard Routes */}
           <Route
