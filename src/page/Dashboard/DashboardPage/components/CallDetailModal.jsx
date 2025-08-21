97d65e5f821482acf2ff2a3de7e18df31c6c81b9
Satendra Kumar <satendrakm27@gmail.com>
2025-08-21 17:31:50 +0530

    color issue fixed

--- src/page/Dashboard/DashboardPage/components/CallDetailModal.jsx
+++ src/page/Dashboard/DashboardPage/components/CallDetailModal.jsx
@@ -88,7 +88,7 @@ const CallDetailModal = ({ isOpen, onClose, callDetail }) => {
                   <span className="text-gray-600">Status:</span>
                   <span
                     className={`px-2 py-1 rounded-full text-xs font-medium capitalize ${
-                      callDetail.status === "completed"
+                      callDetail.status === "Connected"
                         ? "bg-green-100 text-green-800"
                         : "bg-red-100 text-red-800"
                     }`}
