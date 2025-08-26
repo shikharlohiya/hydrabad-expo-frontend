da0ec2fd356f719900fb614a7deb439491cd4aff
Satendra Kumar <satendrakm27@gmail.com>
2025-08-26 17:56:03 +0530

    version increased to 9

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -462,7 +462,7 @@ const Header = ({ collapsed, setCollapsed }) => {
                         {userData?.EmployeeRegion}
                       </p>
                       <p className="text-xs text-gray-600 whitespace-normal break-words">
-                        <span className="font-medium">version:</span> 8.0.0
+                        <span className="font-medium">version:</span> 9.0.0
                       </p>
                     </div>
                   </div>
